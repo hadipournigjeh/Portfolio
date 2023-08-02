b33353367f78aa5a5734f9e3fc77a563598d2c24
Hadi Allahverdi Pournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-08-02 22:20:07 +0100

    I will make toggle icon navbar in js file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// ============= toggle icon navbar =================
+
 // ============= scroll sections active link =================
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
