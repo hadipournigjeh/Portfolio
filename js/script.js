5669fe0b43921b06a8353a5bfab00c00eff77a33
Hadi Allahverdi Pournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-08-08 21:32:09 +0100

    Added distance, duration and delay in scrollReveal

--- js/script.js
+++ js/script.js
@@ -38,4 +38,9 @@ window.onscroll = () => {
 };
 
 // ============= scroll reveal =============================
-ScrollReveal({ reset: true });
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
